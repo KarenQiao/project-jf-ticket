6513f1670d139d8c0acd61d63d0c0e94e40b132e
KarenQiao <498101094@qq.com>
2017-09-25 10:16:31 +0800

    上传gulp配置文件

---
+++ gulp/dev/js.dev.js
@@ -0,0 +1,28 @@
+/**脚本合并
+ * 开发*/
+
+var gulp = require('gulp'),
+
+    concatDir = require('gulp-concat-dir'),//按文件夹合并
+
+    connect = require('gulp-connect'),//服务器
+
+    concat = require("gulp-concat"),//文件合并
+
+    rename = require("gulp-rename");//重命名
+
+
+function devJs() {
+
+    //主要依赖模块
+
+    gulp.src(['src/js/*.js','src/component/**/*.js'])
+
+        .pipe(concat('jf_tickets.js'))
+
+        .pipe(gulp.dest('build/js'))
+
+        .pipe(connect.reload());
+}
+
+module.exports = devJs;

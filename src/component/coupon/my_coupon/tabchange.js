4cb8225471aa8c2b01b5a999a074d083a09ada77
KarenQiao <498101094@qq.com>
2017-09-26 19:54:47 +0800

    提交js

--- src/component/coupon/my_coupon/tabchange.js
+++ src/component/coupon/my_coupon/tabchange.js
@@ -11,17 +11,12 @@ var couponTabChange={
 
         var allTabEle=document.getElementsByClassName('all_coupon_tabs')[0];
 
-
-
-
+        
         for(var i=0;i< clickEle.length;i++){
 
             clickEle[i].addEventListener('click',tabMoveFn,false);
 
-
         }
-
-
 
         function tabMoveFn(e){
 
@@ -35,45 +30,7 @@ var couponTabChange={
 
             var moveDis=-thisWindowWidth*thisIndex;
 
-            console.log(moveDis)
-
-
             allTabEle.style.transform='translate3d('+moveDis+'px,0,0)';
-
-
-
-            /*  var evt=e||window.event;
-
-             var thisTargetEle=evt.srcElement||evt.target;
-
-             if(thisTargetEle.className.indexOf('label_part')>-1){
-
-             document.getElementsByClassName('show')[0].className= document.getElementsByClassName('show')[0].className.replace('show',"")
-
-             thisTargetEle.className=thisTargetEle.className+' show';
-
-             }else {
-             var thisParentEle=thisTargetEle.parentNode;
-
-             for(var i=0;i<20;i++){
-
-             if(thisParentEle.className.indexOf('label_part')>-1){
-
-             document.getElementsByClassName('show')[0].className= document.getElementsByClassName('show')[0].className.replace('show',"")
-
-             thisParentEle.className=thisParentEle.className+' show';
-
-             break
-
-             }else {
-             thisTargetEle= thisParentEle;
-             }
-
-
-             }
-             }
-
-             */
 
         }
         function getRightIndex(){
